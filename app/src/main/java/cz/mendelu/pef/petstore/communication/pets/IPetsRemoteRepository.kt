183e4bebf3828f76eb5d31a9837c6ae11fbd0232
Josef Pavlíček <xpavlic6@mendelu.cz>
2023-10-23 22:00:22 +0200

    lecture 3 - finished pet list endpoint

--- app/src/main/java/cz/mendelu/pef/petstore/communication/pets/IPetsRemoteRepository.kt
+++ app/src/main/java/cz/mendelu/pef/petstore/communication/pets/IPetsRemoteRepository.kt
@@ -1,10 +1,11 @@
 package cz.mendelu.pef.petstore.communication.pets
 
 import cz.mendelu.pef.petstore.architecture.CommunicationResult
+import cz.mendelu.pef.petstore.architecture.IBaseRemoteRepository
 import cz.mendelu.pef.petstore.model.Pet
 import retrofit2.Response
 import retrofit2.http.Query
 
-interface IPetsRemoteRepository {
+interface IPetsRemoteRepository: IBaseRemoteRepository {
     suspend fun findByStatus(status: String): CommunicationResult<List<Pet>>
 }
--- app/src/main/java/cz/mendelu/pef/petstore/communication/pets/PetsRemoteRepositoryImpl.kt
+++ app/src/main/java/cz/mendelu/pef/petstore/communication/pets/PetsRemoteRepositoryImpl.kt
@@ -11,32 +11,11 @@ class PetsRemoteRepositoryImpl
 @Inject constructor(private val petsAPI: PetsAPI) : IPetsRemoteRepository {
 
     override suspend fun findByStatus(status: String): CommunicationResult<List<Pet>> {
-        try {
-            val response = withContext(Dispatchers.IO) {
+        return processResponse(
+            withContext(Dispatchers.IO) {
                 petsAPI.findByStatus(status)
             }
-
-            if (response.isSuccessful) {
-                if (response.body() != null){
-                    // success, send response data
-                    return CommunicationResult.Success(response.body()!!)
-                } else {
-                    // error, send error object with code and description
-                    return CommunicationResult.Error(
-                        cz.mendelu.pef.petstore.architecture.Error(response.code(),
-                            response.errorBody().toString())
-                    )
-                }
-            } else {
-                // communication error, send error object with code and description
-                return CommunicationResult.Error(
-                    cz.mendelu.pef.petstore.architecture.Error(response.code(),
-                        response.errorBody().toString())
-                )
-            }
-        } catch (ex: Exception) {
-            return CommunicationResult.Exception(ex)
-        }
+        )
     }
 
 }
